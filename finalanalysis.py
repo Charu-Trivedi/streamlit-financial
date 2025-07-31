5f5320235cb15e67016db6e66d3467eb1af524e9
Charu-Trivedi <ctrivedi@qcc.mass.edu>
2025-07-31 12:21:52 -0400

    Update finalanalysis.py

--- finalanalysis.py
+++ finalanalysis.py
@@ -3,14 +3,14 @@ import pandas as pd
 
 import streamlit as st # add at top with other imports, the rest at the bottom
 def fetch_data(tickers, start_date, end_date):
-data_frames = []
-for ticker in tickers:
-stock_data = yf.download(ticker, start=start_date, end=end_date)['Close']
-data_frames.append(stock_data)
+  data_frames = []
+  for ticker in tickers:
+    stock_data = yf.download(ticker, start=start_date, end=end_date)['Close']
+    data_frames.append(stock_data)
 
-data = pd.concat(data_frames, axis=1, keys=tickers)
-data.columns = tickers
-return data
+  data = pd.concat(data_frames, axis=1, keys=tickers)
+  data.columns = tickers
+  return data
 
 st.title("Financial Analysis Web App")
 # User inputs
